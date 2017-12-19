874b2c49d2819c98ce433c0d5a7185b5caa82602
ohguanri <ohguanri@gmail.com>
2017-12-20 00:18:42 +0800

    Changes
    added a @Post method in LoginRestful
    added a $http.post in controller.js
    added parameters to submit() in index.html

--- src/com/demo/controller/LoginRestful.java
+++ src/com/demo/controller/LoginRestful.java
@@ -2,6 +2,7 @@ package com.demo.controller;
 
 import java.util.ArrayList;
 
+import javax.json.JsonObject;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -13,28 +14,38 @@ import com.demo.model.User;
 @Path("login")
 public class LoginRestful {
 	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)		// to return as JSON
-	@Path("/showUsers")
-	public ArrayList<User> listUsers(){
-		ArrayList<User> users = new ArrayList<>();
-		
-		User user1 = new User();
-		user1.setUserName("admin");
-		user1.setPassword("admin");
-		
-		users.add(user1);
-		
-		return users;
-	}
+//	@GET
+//	@Produces(MediaType.APPLICATION_JSON)		// to return as JSON
+//	@Path("/showUsers")
+//	public ArrayList<User> listUsers(){
+//		ArrayList<User> users = new ArrayList<>();
+//		
+//		User user1 = new User();
+//		user1.setUserName("admin");
+//		user1.setPassword("admin");
+//		
+//		User user2 = new User();
+//		user2.setUserName("user");
+//		user2.setPassword("user");
+//		
+//		users.add(user1);
+//		users.add(user2);
+//		
+//		return users;
+//	}
 	
 	@POST
 	@Path("/authenticate")
-	public boolean authenticate(String userName, String password) {
+	public boolean authenticate(JsonObject jData) {
+		String userName = jData.getString("userName");
+		String password = jData.getString("password");
+		
 		
 		if (userName.equals("admin")  && password.equals("admin")) {
+			System.out.println(true);
 			return true;
 		} else {
+			System.out.println(false);
 			return false;
 		}
 	}
